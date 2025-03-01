7bfba04777bcda491c5872e0d4f244c310f29822
md <marceldiomare@gmail.com>
2025-03-01 22:15:15 +0300

    008 added cart urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from shop.views import CategoryListView, ProductListView
+from shop.views import CartView, AddToCartView, UpdateCartView, RemoveFromCartView, ClearCartView
 
 urlpatterns = [
     path("categories/", CategoryListView.as_view(), name="category-list"),
     path("products/", ProductListView.as_view(), name="product-list"),
+
+    path("cart/", CartView.as_view(), name="cart-view"),
+    path("cart/add/", AddToCartView.as_view(), name="add-to-cart"),
+    path("cart/update/<int:product_id>/", UpdateCartView.as_view(), name="update-cart"),
+    path("cart/remove/<int:product_id>/", RemoveFromCartView.as_view(), name="remove-from-cart"),
+    path("cart/clear/", ClearCartView.as_view(), name="clear-cart"),
 ]
